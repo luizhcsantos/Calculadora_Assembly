40bf9ac8b15dc7052a6babd06834851dde5227cf
luizhcsantos <lh.santos2008@gmail.com>
2024-05-20 17:02:59 -0300

    Criacao do projeto

---
+++ unit1.pas
@@ -0,0 +1,216 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+{$ASMMODE intel}
+
+uses
+  Classes, SysUtils, StrUtils, Forms, Controls, Graphics, Dialogs, StdCtrls;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Button1: TButton;
+    Button10: TButton;
+    Button11: TButton;
+    Button12: TButton;
+    Button13: TButton;
+    Button14: TButton;
+    Button15: TButton;
+    Button16: TButton;
+    Button17: TButton; // botão de '='
+    Button18: TButton;
+    Button19: TButton;
+    Button2: TButton;
+    Button20: TButton;
+    Button21: TButton;
+    Button22: TButton;
+    Button23: TButton;
+    Button24: TButton;
+    Button25: TButton;
+    Button26: TButton;
+    Button27: TButton;
+    Button28: TButton;
+    Button29: TButton;
+    Button3: TButton;
+    Button30: TButton;
+    Button31: TButton;
+    Button32: TButton;
+    Button4: TButton;
+    Button5: TButton;
+    Button6: TButton;
+    Button7: TButton;
+    Button8: TButton;
+    Button9: TButton;
+    CheckBox1: TCheckBox;
+    Edit1: TEdit;
+    RadioButton1: TRadioButton;
+    RadioButton2: TRadioButton; 
+    procedure FormCreate(Sender: TObject);
+    procedure ButtonClick(Sender: TObject);
+    procedure ButtonEqualsClick(Sender: TObject);    
+    procedure ButtonClearClick(Sender: TObject);
+
+
+  private
+    { Private declarations }
+    ExpressaoNP: string;
+    function AvaliarNP(expressao: string): Double;
+    function transformaNP(infixa: string): string;
+    function AvaliarNP(np: string): Double;
+
+  public
+    { Public declarations }
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+type TPilha = record
+      dado: array of Double;
+      topo: Integer;
+      end;
+
+// Operações da Pilha
+//*******************
+// Inicializar a pilha
+procedure IniciarPilha(var stack: TPilha);
+begin
+  SetLength(stack.dado, 0);
+  stack.topo := -1;
+end;
+
+// Colocar valores no topo da pilha
+procedure Push(var stack: TPilha; valor: Double);
+begin
+  Inc(stack.topo);
+  SetLength(stack.dado, stack.topo + 1);
+  stack.dado[stack.topo] := valor;
+end;
+
+// Retirar valores no topo da pilha
+function Pop(var stack: TPilha) : Double;
+begin
+    if stack.topo < 0 then
+    raise Exception.Create('Stack underflow');
+    Result := stack.dado[stack.topo];
+    Dec(stack.topo);
+end;
+
+function ehOperador(s: string): Boolean;
+begin
+    Result := (s = '+') or (s = '-') or (s = '*') or (s = '/');
+end;
+
+
+// Esta função recebe uma expressão na Notação Polonesa (NP)
+// divide a expressão em tokens
+// e processa cada token.
+function TForm1.AvaliarNP(expressao: string): Double;
+var
+  pilha: TPilha;
+  tokens: TStringArray;
+  token: string;
+  op1, op2: Double;
+
+  begin
+      IniciarPilha(pilha);
+      tokens := SplitString(expressao, ' ');
+
+      for token in tokens do
+      begin
+        if TryStrToFloat(token, op1) then // se o token é um número é colocado na pilha
+        begin
+          Push(pilha, op1);
+        end
+        else if ehOperador(token) then
+        // se o token é um operador, dois operandos são retirados da pilha
+        // e a operação é realizada em assembly
+        begin
+          op2 := Pop(pilha);
+          op1 := Pop(pilha);
+
+          if token = '+' then
+          asm
+             fld op1
+             fadd op2
+             fstp op1
+          end
+          else if token = '-' then
+          asm
+             fld op1
+             fsub op2
+             fstp op1
+          end
+          else if token = '*' then
+          asm
+             fld op1
+             fmul op2
+             fstp op1
+          end
+          else if token = '/' then
+          asm
+             fld op1
+             fdiv op2
+             fstp op1
+          end;
+
+          Push(pilha, op1);
+      end
+      else
+      begin
+        raise Exception.Create('Token Invalido: ' + token);
+      end;
+     end;
+
+      Result := Pop(pilha);
+end;
+
+procedure TForm1.ButtonClick(Sender: TObject);
+begin
+  // Adiciona o texto do botão à expressão
+  ExpressaoNP := ExpressaoNP + (Sender as TButton).Caption + ' ';
+  Edit1.Text := ExpressaoNP;
+end;
+
+procedure TForm1.ButtonEqualsClick(Sender: TObject);
+var
+  resultado: Double;
+begin
+    try
+      resultado := AValiarNP(ExpressaoNP);
+      Edit1.Text := FloatToStr(resultado);
+      ExpressaoNP := ''; // Reseta a expressão após calcular
+    except
+      on E: Exception do
+         Edit1.Text := 'Error: ' + E.Message;
+    end;
+end;
+
+procedure TForm1.FormCreate(Sender: TObject);
+var
+  i: Integer;
+  btn: TButton;
+begin
+  // Associa o evento ButtonClick a todos os botões de numeros e operadores
+  for i := 1 to 32 do
+  begin
+    btn := TButton(FindComponent('Button' + IntToSTr(i)));
+    if Assigned(btn) then
+       btn.OnClick := @ButtonClick;
+  end;
+  Button17.OnClick := @ButtonEqualsClick;
+end;
+
+
+end.
+
