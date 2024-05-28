1c0d3f2fd7d81e7e17b56ef337bcbcca4e143a6d
luizhcsantos <lh.santos2008@gmail.com>
2024-05-28 16:33:29 -0300

    Raiz quadrada calculada corretamente

--- backup/unit1.pas
+++ backup/unit1.pas
@@ -90,8 +90,9 @@ type
 
     function InfixToPostfix(expression: string): string;
     function ehLetraOuDigito(c: Char): Boolean;
-    function Precedence(op: char): integer;
+    function Precedence(op: string): integer;
     function ehOperador(s: string): Boolean;
+
 
   public
     { Public declarations }
@@ -130,11 +131,12 @@ begin
   Edit1.Text := '';
 end;
 
-function TForm1.Precedence(op: char): integer;
+function TForm1.Precedence(op: string): integer;
 begin
   case op of
     '~': Precedence := 6;
     '^': Precedence := 5;
+    's': Precedence := 5;
     '*', '/': Precedence := 4;
     '+', '-': Precedence := 3;
     '<', '>': Precedence := 2;
@@ -151,17 +153,36 @@ var
   i: integer;
   ch: char;
   tempNum: string;
-  tempStr: string;
+  tempOp: string;
+  operacoesEspeciais: array of string;
+
+  function ehOperacaoEspecial(op: string): Boolean;
+  var
+    j: Integer;
+  begin
+    Result := False;
+    for j := Low(operacoesEspeciais) to High(operacoesEspeciais) do
+    begin
+      if op = operacoesEspeciais[j] then
+      begin
+        Result := True;
+        Break;
+      end;
+    end;
+  end;
+
 begin
+  // Lista de operações especiais
+  operacoesEspeciais := ['sqrt', 'log', 'sin', 'cos'];
   IniciarPilhaChar(P1);
   L1 := TStringList.Create;
   tempNum := '';
+  tempOp := '';
   try
     i := 1;
     while i <= Length(expression) do
     begin
       ch := expression[i];
-
       if ch in ['0'..'9', '.'] then
       begin
         tempNum := tempNum + ch; // Acumula o número
@@ -170,63 +191,66 @@ begin
       begin
         if tempNum <> '' then
         begin
-          L1.Add(tempNum); // Adiciona o número acumulado à lista
+          L1.Add(tempNum); // Adiciona o numero acumulado à lista
           tempNum := '';
         end;
 
-        if (ch in ['a'..'z', 'A'..'Z']) then
+      if ch in ['a'..'z', 'A'..'Z'] then
+      begin
+        tempOp := tempOp + ch;
+        // Verifica se é uma operação especial completa
+        if ehOperacaoEspecial(tempOp) then
         begin
-          tempStr := '';
-          while (i <= Length(expression)) and (expression[i] in ['a'..'z', 'A'..'Z']) do
-          begin
-            tempStr := tempStr + expression[i];
-            Inc(i);
-          end;
-          Dec(i); // Compensar o incremento extra
-          if tempStr = 'sqrt' then
-          begin
-            L1.Add(tempStr);
-          end
-          else
-          begin
-            raise Exception.Create('Operador inválido: ' + tempStr);
+          PushChar(P1, tempOp[1]); // Empilha um '(' para a operação especial
+          //L1.Add(tempOp);    // Adiciona a operação especial à lista
+          tempOp := '';
           end;
         end
-        else
+      else
+      begin
+        if tempOp <> '' then
         begin
-          case ch of
-            '(':
-              PushChar(P1, ch);
-            ')':
-              begin
-                while (not PilhaCharVazia(P1)) and (P1.dado[P1.topo] <> '(') do
-                begin
-                  L1.Add(PopChar(P1));
-                end;
-                if (not PilhaCharVazia(P1)) then
-                  PopChar(P1); // Remover '(' da pilha
-              end;
-            '+', '-', '*', '/', '^', '~':
-              begin
-                while (not PilhaCharVazia(P1)) and (Precedence(P1.dado[P1.topo]) >= Precedence(ch)) do
-                begin
-                  L1.Add(PopChar(P1));
-                end;
-                PushChar(P1, ch);
-              end;
-          else
-            raise Exception.Create('Caractere inválido: ' + ch);
+          L1.Add(tempOp);  // Adiciona a operação acumulada à lista
+          tempOp := '';
           end;
-        end;
+        case ch of
+          '(':
+            PushChar(P1, ch);
+          ')':
+            begin
+              while (not PilhaCharVazia(P1)) and (P1.dado[P1.topo] <> '(') do
+              begin
+                L1.Add(PopChar(P1));
+              end;
+              if (not PilhaCharVazia(P1)) then
+                PopChar(P1); // Remover '(' da pilha
+              if (not PilhaCharVazia(P1)) and (P1.dado[P1.topo] in ['s', 'l', 'c']) then
+              begin
+                L1.Add(PopChar(P1)); // Adiciona a operação especial
+                end;
+            end;
+          '+', '-', '*', '/', '^', '~':
+            begin
+              while (not PilhaCharVazia(P1)) and (Precedence(P1.dado[P1.topo]) >= Precedence(ch)) do
+              begin
+                L1.Add(PopChar(P1));
+              end;
+              PushChar(P1, ch);
+            end;
+          end;
       end;
-      Inc(i);
+    end;
+    Inc(i);
     end;
 
     if tempNum <> '' then
     begin
       L1.Add(tempNum); // Adiciona o último número acumulado, se houver
     end;
-
+    if tempOp <> '' then
+    begin
+      L1.Add(tempOp);  // Adiciona a última operação acumulada, se houver
+      end;
     while (not PilhaCharVazia(P1)) do
     begin
       L1.Add(PopChar(P1));
@@ -245,16 +269,21 @@ var
   textoAtual: string;
   numeroDigitado: string;
   tamanho: Integer;
+  i: Integer;
+  operacoesEspeciais: array of string;  
+  isOperacaoEspecial: Boolean;
 begin
+  // Lista de operações especiais
+  operacoesEspeciais := ['sqrt', 'log', 'sin', 'cos'];
+
   // Obtenha o dígito do botão clicado
   numeroDigitado := (Sender as TButton).Caption;
-
 
   // Verifique se o texto atual do Edit1 é vazio
   if Edit1.Text = '' then
   begin
     // Se o texto estiver vazio, apenas adicione o dígito ao Edit1
-    Edit1.Text := numeroDigitado;
+    Edit1.Text := numeroDigitado + ' ';
     Exit;
   end;
 
@@ -262,10 +291,25 @@ begin
   // Vamos verificar se o último caractere é um operador
   textoAtual := TrimRight(Edit1.Text);
   tamanho := Length(textoAtual);
-  if (ehOperador(numeroDigitado)) then
+
+  isOperacaoEspecial := False;
+  for i := Low(operacoesEspeciais) to High(operacoesEspeciais) do
   begin
+    if numeroDigitado = operacoesEspeciais[i] then
+    begin
+      numeroDigitado := operacoesEspeciais[i] + ' ';
+      isOperacaoEspecial := True;
+      Break;
+    end;
+  end;
+
+  if not isOperacaoEspecial and ehOperador(numeroDigitado) then
+  begin
+    // Se for outro operador, adicione um espaço antes do operador
     numeroDigitado := ' ' + numeroDigitado;
   end;
+
+
   if textoAtual[tamanho] in ['+', '-', '*', '/', '~'] then
   begin
     // Se o último caractere for um operador, adicione um espaço antes de adicionar o próximo dígito
@@ -286,7 +330,7 @@ var
   pilha: TPilhaDouble;
   tokens: TStringArray;
   token: string;
-  op1, op2, resultado: Double;
+  op1, op2: Double;
   foundNil: Boolean;
   base: Integer;
 begin
@@ -294,7 +338,7 @@ begin
   tokens := SplitString(expressao, ' ');
 
   foundNil := False;
-  base := 10;
+
   for token in tokens do
   begin
     if token = '' then
@@ -308,63 +352,66 @@ begin
     end
     else if ehOperador(token) then
     begin
-      if (token <> 'sqrt') then
+      if (token = '~') or (token = 's') or (token = 'l') then // Operações que requerem dois operandos
       begin
         op1 := PopDouble(pilha);
+
+        if token = '~' then
         asm
-         fld op1
-         fsqrt
-         fstp op1
-        end;
-      end
-       else if token = '~' then
-       begin
-       // se o token é '~' , apenas um operando é retirado da pilha
-       // e a operação de negativo é realizada em assembly
-         op1 := PopDouble(pilha);
-         asm
           fld op1
           fchs
           fstp op1
         end
-       end;
-      if token = '+' then
-      asm
-        fld op1
-        fadd op2
-        fstp op1
-      end
-      else if token = '-' then
-      asm
-        fld op1
-        fsub op2
-        fstp op1
-      end
-      else if token = '*' then
-      asm
-        fld op1
-        fmul op2
-        fstp op1
-      end
-      else if token = '/' then
-      asm
-        fld op1
-        fdiv op2
-        fstp op1
-      end
-      else if token = 'log' then
-      asm
-       finit //inicializa a pilha
-       fld1 //[ 1.0 ]
-       fld base //[ n ; 1.0 ] // para fazer 1 * log2n
-       fyl2x //[ log2n ] // st = st(1).log2(st)
-       fld1 //[ 1.0 ; log2n ]
-       fdiv st, st(1) //[ 1.0 / log2n ]
-       fld op1 //[ x ; 1.0 / log2n ]
-       fyl2x //[1.0 / log2n * log2x ] // st = st(1).log2(st)
-       fstp op1
-       end;
+        else if token = 'sqrt' then
+        asm
+         fld op1
+         fsqrt
+         fstp op1
+        end
+        else if token = 'log' then
+            asm
+             finit //inicializa a pilha
+             fld1 //[ 1.0 ]
+             fld base //[ n ; 1.0 ] // para fazer 1 * log2n
+             fyl2x //[ log2n ] // st = st(1).log2(st)
+             fld1 //[ 1.0 ; log2n ]
+             fdiv st, st(1) //[ 1.0 / log2n ]
+             fld op1 //[ x ; 1.0 / log2n ]
+             fyl2x //[1.0 / log2n * log2x ] // st = st(1).log2(st)
+             fstp op1
+            end
+       end
+       else // Operações que requerem dois operandos
+       begin
+            op2 := PopDouble(pilha);
+            op1 := PopDouble(pilha);
+            base := 10;
 
+            if token = '+' then
+            asm
+              fld op1
+              fadd op2
+              fstp op1
+            end
+            else if token = '-' then
+            asm
+              fld op1
+              fsub op2
+              fstp op1
+            end
+            else if token = '*' then
+            asm
+              fld op1
+              fmul op2
+              fstp op1
+            end
+            else if token = '/' then
+            asm
+              fld op1
+              fdiv op2
+              fstp op1
+            end;
+       end;
       PushDouble(pilha, op1);
     end
     else
@@ -472,7 +519,7 @@ end;
 
 function TForm1.ehOperador(s: string): Boolean;
 begin
-  Result := (s = '+') or (s = '-') or (s = '*') or (s = '/') or (s = '~') or (s = 'sqrt');
+  Result := (s = '+') or (s = '-') or (s = '*') or (s = '/') or (s = '~') or (s = 's') or (s = 'l');
 end;
 
 function TForm1.ehLetraOuDigito(c: Char): Boolean;
